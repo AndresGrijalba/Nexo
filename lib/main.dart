f414a7e94addaee317a2f588ce8a14c628cebca3
AndresGrijalba <kevingr2508@gmail.com>
2025-09-29 14:47:30 -0500

    Bug fix and keep theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app.dart';
+import 'core/theme/theme_controller.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ThemeController.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
